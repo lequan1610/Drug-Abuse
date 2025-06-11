2c36eea84a1d2083fb9536f12b0a01fca06d63e3
lequan1610 <lequan16102006@gmail.com>
2025-06-11 17:06:13 +0200

    Update Drug-Abuse-1.R

--- Drug-Abuse-1.R
+++ Drug-Abuse-1.R
@@ -1,5 +1,6 @@
 library(readr)
-score_mdma_2011_2017 <- read_delim("C:/Users/lequa/Downloads/score_mdma_wastewater_loads_2011_2017.csv", 
-delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#score_mdma_2011_2017 <- read_delim("C:/Users/lequa/Downloads/score_mdma_wastewater_loads_2011_2017.csv", 
+#delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
-write.csv(score_mdma_2011_2017, "data/scoremdma20112017.csv")
+#write.csv(score_mdma_2011_2017, "data/scoremdma20112017.csv")
+scoremdma20112017 <- read_csv("data/scoremdma20112017.csv")
