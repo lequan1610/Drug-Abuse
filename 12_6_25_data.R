ec6e287dcadf4898d3238aaff6b969e7eaa9535f
lequan1610 <lequan16102006@gmail.com>
2025-06-12 17:24:14 +0200

    Update 12_6_25_data.R

--- 12_6_25_data.R
+++ 12_6_25_data.R
@@ -50,7 +50,9 @@ df_long <- df %>%
 library(ggplot2)
 
 df_plot_utr <- df_long %>% filter(city == "Utrecht")
-
+df_plot_eid <- df_long %>% filter(city == "Eindhoven")
+ggplot(data = df_plot_eid, aes(x = year, y = mean))+
+  geom_line(data = df_plot_eid, aes(x = year, y = mean, colour = "green", group=1))
 #xValue <- df_long[1:7,4]
 #yValue <- df_long[1:7,6]
 #data <- data.frame(xValue, yValue)
@@ -63,12 +65,13 @@ ggplot(df_plot, aes(x = year, y = mean)) +
 
 df_plot_ams <- df_long %>% filter(city == "Amsterdam")
 ggplot(data = df_plot_utr, aes(x = year, y = mean)) +
-  geom_point(data = df_plot_utr, aes(x = year, y = mean, colour = "red")) +
+  geom_line(data = df_plot_utr, aes(x = year, y = mean, colour = "red", group=1)) +
   geom_point(data = df_plot_ams, aes(x = year, y = mean, colour = "blue",)) +
+  geom_line(data = df_plot_eid, aes(x = year, y = mean, colour = "green", group=1)) +
   scale_color_identity(name="",
-                       breaks=c("red","blue"
+                       breaks=c("red","blue","green"
                                 ),
-                       labels=c("Utrecht", "Amsterdam"),
-                       guide = "legend")
+                       labels=c("Utrecht", "Amsterdam","Eindhoven"),
+                       guide="legend")
 
 
